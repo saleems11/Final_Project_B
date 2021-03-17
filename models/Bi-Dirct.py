87b299ca0e45c53552e4a9551879dcf24ef7aa91
saleems11 <iamme0ssa@gmail.com>
2021-03-17 18:49:24 +0200

    created the lstm model it still need some modification and addetioanl care about the validation data(the accuracy is very low or unmatched, it changes according to the loss function)

--- models/Bi-Dirct.py
+++ models/Bi-Dirct.py
@@ -42,9 +42,9 @@ def train_model(model, x_train, y_train,itterations):
 
 def load_data():
 	c1 = Documents_utils.get_list_of_books(Documents_utils.c1)
-	c1 = c1[:min(3, len(c1))]
+	c1 = c1[:min(35, len(c1))]
 	c2 = Documents_utils.get_list_of_books(Documents_utils.c2)
-	c2 = c2[:min(1, len(c2))]
+	c2 = c2[:min(10, len(c2))]
 	c3 = Documents_utils.get_list_of_books(Documents_utils.c3)
 	c3 = c2[:min(1, len(c3))]
 
@@ -60,27 +60,30 @@ def load_data():
 	embedded_data_c3 = Emb_D.Embedd_DataSet.embedd_Aravec(books=c3, tweet_size=100)
 	print(embedded_data_c3.shape)
 
+	return embedded_data_c1,embedded_data_c2,embedded_data_c3
+
+
+def create_new_batch(embedded_data_c1, embedded_data_c2):
 	# Now lets balance the data
 	s1, s2 = BR.Balancing_DataSet.Balancing_Routine(embedded_data_c1,
 													embedded_data_c2,
 													3,
 													2)
-	return s1,s2,c3
+
+	x_train = np.concatenate((s1, s2))
+	# the first value is gazali, the sec is psedo
+	y1 = np.tile(np.array([1, 0]), (len(s1), 1))
+	y2 = np.tile(np.array([0, 1]), (len(s2), 1))
+	y_train = np.concatenate((y1, y2))
+
+	return x_train, y_train
 
 
-embedding_size= 100
+embedding_size = 100
 tweet_lenght = 100
+
 # the data need to be normlized
-s1,s2,c3 = load_data()
-x_train = np.concatenate((s1,s2))
-# the first value is gazali, the sec is psedo
-y1 = np.tile(np.array([1,0]),(len(s1),1))
-y2 = np.tile(np.array([0,1]),(len(s2),1))
-y_train = np.concatenate((y1,y2))
-
-x_test = c3
-
-
+c1, c2, c3 = load_data()
 
 """ creatin the model"""
 bi_lstm_hidden_state_size = 200
@@ -91,15 +94,22 @@ model.add(Bidirectional(
 	input_shape=(tweet_lenght, embedding_size),
 	merge_mode="concat"))
 
-model.add(Dropout(0.2))
-model.add(Dense(2*bi_lstm_hidden_state_size, activation='relu'))
-model.add(Dropout(0.2))
+model.add(Dropout(0.4))
+model.add(Dense(30, activation='relu'))
+model.add(Dropout(0.4))
 model.add(Dense(2, activation='relu'))
 
 print(model.summary())
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=10, batch_size=50, verbose=0)
+for i in range(0,20):
+	x_train,y_train =create_new_batch(c1, c2)
+	history = model.fit(x_train, y_train, validation_split=.20, epochs=10, batch_size=50, verbose=1)
+
+	print(history.history['accuracy'])
+	# Doesn't work
+	if history.history['accuracy'][len(history.history['accuracy'])-1]>0.5:
+		pass
 
 results = DataFrame()
 # results['bilstm_con'] = train_model(model, x_train, y_train, 20)
