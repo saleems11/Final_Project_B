4fbaad454028d0c7f9ad5f4f59459444fb813d4d
saleems11 <iamme0ssa@gmail.com>
2021-05-03 18:13:27 +0300

    added feague feture in heatMap

--- Show_results/Heat_map.py
+++ Show_results/Heat_map.py
@@ -4,7 +4,9 @@ import matplotlib.pylab as plt
 import numpy as np
 
 import tkinter
+from tkinter import Frame
 
+from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
@@ -23,23 +25,33 @@ class Heat_map:
 
 
     @staticmethod
-    def create_figure(data) -> Figure:
+    def create_figure(data, dpi) -> Figure:
         # plot the data
-        figure = Figure(figsize=(8, 10))
+        figure = Figure(dpi=100)
         ax = figure.subplots()
         # call creating heat map
         Heat_map.create_heat_map(data=data, ax=ax)
         return figure
 
     @staticmethod
-    def create_GUI(root, data):
-        sns.set()
-        init_figure = Heat_map.create_figure(data=data)
-        canvas = FigureCanvasTkAgg(init_figure, master=root)
-        canvas.figure = init_figure
+    def create_GUI(main_frame, data):
+        # create two frames
+        top_frame = Frame(main_frame)
+        top_frame.grid(row=0, column=0, sticky="nswe")
+        bottom_frame = Frame(main_frame)
+        bottom_frame.grid(row=1, column=0, sticky="nswe")
+
+        dpi = top_frame.winfo_fpixels('1i')
+        init_figure = Heat_map.create_figure(data=data, dpi=dpi)
+        canvas = FigureCanvasTkAgg(init_figure, master=top_frame)
         canvas.draw()
-        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-        return canvas
+        # canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
+        canvas.get_tk_widget().grid(sticky=tkinter.NSEW)
+        # add tool bar
+
+        toolbar = NavigationToolbar2Tk(canvas, bottom_frame)
+        canvas._tkcanvas.grid()
+        toolbar.update()
 
 
     @staticmethod
@@ -54,8 +66,9 @@ class Heat_map:
 
     @staticmethod
     def _generate_sample_data():
-        M = np.array([1, 10, 12, 16, 1, 18, 8, 9, 1, 12, 16, 1, 18, 8, 2])
-        iteration_size = 3
+        M = np.array([1, 10, 12, 16, 1, 18, 8, 9, 1, 12, 16, 1, 18, 8, 2,
+                      2, 1, 5, 9, 2, 1, 2, 4, 9, 2, 7, 9, 3, 4, 1])
+        iteration_size = 10
         return M, iteration_size
 
 if __name__ == "__main__":
